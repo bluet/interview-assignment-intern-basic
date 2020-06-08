0852c0d61fa8190ad315e733698845397d388c7d
BlueT - Matthew Lien - 練喆明 <BlueT@BlueT.org>
2020-06-08 15:39:08 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,69 @@
 module.exports = {
-	'env': {
-		'es6': true,
-		'node': true
+	env: {
+		"es6": true,
+		"node": true,
+		"browser": true,
+		"es2020": true,
+		"mongo": true,
+		"shared-node-browser": true,
 	},
-	'extends': 'eslint:recommended',
-	'parserOptions': {
-		'ecmaVersion': 2017,
-		'sourceType': 'module'
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: 11,
+		sourceType: "module",
 	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab',
-			{ 'SwitchCase': 1 },
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'warn',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'no-console': [
-			'warn',
-			{ allow: ['warn', 'error', 'info'] }
-		],
-		'brace-style': [
-			'error',
-			'1tbs',
-			{ 'allowSingleLine': true }
-		],
-		'no-unused-vars': [
-			'warn',
+	rules: {
+		"indent": [
+			"error",
+			"tab",
 			{
-				'vars': 'all',
-				'args': 'after-used',
-				'ignoreRestSiblings': false
-			}
+				VariableDeclarator: "first",
+			},
 		],
-		'no-mixed-spaces-and-tabs': [
-			'error',
-			'smart-tabs'
-		]
-	}
+		"linebreak-style": ["error", "unix"],
+		"curly": "error",
+		"quotes": ["error", "double"],
+		"semi": ["error", "always"],
+		"no-console": [
+			"warn",
+			{
+				allow: ["warn", "error", "info"],
+			},
+		],
+		"brace-style": [
+			"error",
+			"1tbs",
+			{
+				allowSingleLine: true,
+			},
+		],
+		"no-unused-vars": [
+			"warn",
+			{
+				vars: "all",
+				args: "after-used",
+				ignoreRestSiblings: false,
+			},
+		],
+		"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+		"no-trailing-spaces": "error",
+		"arrow-parens": ["error", "always"],
+		"arrow-body-style": ["warn", "always"],
+		"func-style": [
+			"warn",
+			"declaration",
+			{
+				allowArrowFunctions: true,
+			},
+		],
+		"no-confusing-arrow": [
+			"error",
+			{
+				allowParens: false,
+			},
+		],
+		"arrow-spacing": "error",
+		"prefer-arrow-callback": "warn",
+		"require-await": "error",
+	},
 };
